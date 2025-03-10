4a6e5f8d709317ec9d936a95dda32c694fbaf77d
mustafiz-jr <mustafijjr@gmail.com>
2025-03-10 23:28:40 +0600

    s

--- index.php
+++ index.php
@@ -6,8 +6,21 @@
     <title>Information collection Form</title>
 <!--Bootstrap link-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<style>
+    input:focus{
+        outline:none;
+    }
+</style>
 </head>
 <body>
-    
+    <main>
+        <section>
+            <h1 class="fs-1 my-5 text-center">Information collection Form </h1>
+            <form action="" method="post" class="container p-5 border rounded-3 border-dark">
+                <input type="text" name="name" placeholder="Enter your name in here!" class=" fs-5 py-2 w-100 border-0 border-bottom ">
+            </form>
+        </section>
+    </main>
 </body>
 </html>
