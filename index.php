ed147c61472975c2810d6d348090c8f18a653674
mustafiz-jr <mustafijjr@gmail.com>
2025-03-11 12:35:23 +0600

    s

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <!--Bootstrap link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+
+    <!-- sweet alert css cdn -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.17.2/dist/sweetalert2.min.css">
     <style>
         input:focus {
             outline: none;
@@ -19,7 +22,7 @@
     <main>
         <section>
             <h1 class="fs-1 my-5 text-center">Information collection Form </h1>
-            <form action="action/sent.php" method="post" class="container p-5 border rounded-3 border-dark">
+            <form action="action/sent.php" method="post" class="container p-5 border rounded-3 border-dark" id="form">
                 <input type="text" name="name" placeholder="Enter your name in here!" class=" fs-5 p-2 w-100 border-0 border-bottom ">
                 <br>
                 <br>
@@ -42,18 +45,38 @@
                 <br>
                 <div class="d-flex gap-5">
                     <button type="submit" class="btn btn-primary btn-md px-3">submit</button>
-        <a href="report.php" class="btn btn-secondary px-4 py-1">Go to Report page</a>
+                    <a href="report.php" class="btn btn-secondary px-4 py-1">Go to Report page</a>
                 </div>
             </form>
         </section>
     </main>
 
+    <!-- sweet alert js cdn -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.17.2/dist/sweetalert2.all.min.js"></script>
 
     <script>
         const form = document.getElementById("form");
 
-        form.addEventListener("submit",function(pause){
+        form.addEventListener("submit", function(pause) {
             pause.preventDefault();
+            const sweet = Swal.fire({
+                title: "Are you sure to submit?",
+                // text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Submit!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                    form.submit();
+                }
+            });
 
         })
     </script>
